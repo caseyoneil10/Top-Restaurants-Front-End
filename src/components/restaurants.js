333f9d536c3824772873a5128976121f294c3818
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-05-31 13:25:56 -0700

    changed functions to match schema

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -4,12 +4,11 @@ const Restaurants = (props) => {
 				<div>
 				<ul>
 					<li>{props.restaurants.name}</li>
-					<li>{props.restaurants.type}</li>
-					<li>{props.restaurants.price}</li>
-					<li>{props.restaurants.rank}</li>
-					<li>{props.restaurants.image}</li>
-					<li>{props.restaurants.hours}</li>
 					<li>{props.restaurants.address}</li>
+					<li>{props.restaurants.type}</li>
+					<li>{props.restaurants.chef}</li>
+					<li>{props.restaurants.rank}</li>
+					<img src={props.restaurants.image}/>
 				</ul>
 					<button onClick={(event) => {
 						props.handleDelete(props.restaurants)
@@ -19,20 +18,19 @@ const Restaurants = (props) => {
 					}}>Edit Info</button>
 				</div>
 				<div>
-				{props.restaurants.show ? 
+				{props.restaurants.show ?
 				<form onSubmit={(event) => {
 						props.handleRestaurantUpdate(event, props.restaurants)
 				}}>
-					<input placeholder='Restaurant Name' onChange={props.handleNewName}></input>
-					<input placeholder='Address' onChange={props.handleNewAddress}></input>
-					<input placeholder='Type of Food' onChange={props.handleNewType}></input>
-					<input placeholder='Image Link' onChange={props.handleNewImage}></input>
-					<input placeholder='Hours' onChange={props.handleNewHours}></input>
-					<input placeholder='Rank' onChange={props.handleNewRank}></input>
-					<input placeholder='Price' onChange={props.handleNewPrice}></input>
+					<input onChange={props.handleNewName} defaultValue={props.restaurants.name}></input>
+					<input defaultValue={props.restaurants.address} onChange={props.handleNewAddress}></input>
+					<input defaultValue={props.restaurants.type} onChange={props.handleNewType}></input>
+					<input defaultValue={props.restaurants.image} onChange={props.handleNewImage}></input>
+					<input defaultValue={props.restaurants.rank} onChange={props.handleNewRank}></input>
+					<input defaultValue={props.restaurants.chef} onChange={props.handleNewChef}></input>
 					<input type='submit' value='Submit'></input>
 					</form> : null }
-					
+
 				</div>
 				</main>
 		)
