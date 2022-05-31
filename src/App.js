0df6ec7ee4c099aa8bfdce3e18e2a592df1450f8
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-05-31 09:11:07 -0700

    basic functionality of react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-
-
-
 import axios from 'axios'
 import {useState, useEffect} from 'react'
+import Restaurants from './components/restaurants'
 
 
 
 const App = () => {
 
+		const [newName, setNewName] = useState()
+		const [newAddress, setNewAddress] = useState()
+		const [newType, setNewType] = useState()
+		const [newPrice, setNewPrice] = useState()
+		const [newImage, setNewImage] = useState()
+		const [newHours, setNewHours] = useState()
+		const [newRank, setNewRank] = useState()
+		const [restaurant, setRestaurant] = useState([])
 
+
+
+		useEffect(() => {
+		        axios.get('http://localhost:3000/restaurants').then((response) => {
+		          setRestaurant(response.data)
+		        })
+
+		      }, [])
 
 return (
+	<>
+	<div>
+	<h1>Top Restaurants in The World</h1>
+	</div>
+	{restaurant.map((restaurants) => {
+			return<Restaurants restaurants={restaurants}/>
+	})}
 
-	<h1>hello world</h1>
+</>
 
 )
 
