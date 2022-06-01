bd02f16dcac7568d0028255cd94c428203b0b31a
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-06-01 13:30:52 -0700

    small styling changes

--- src/App.js
+++ src/App.js
@@ -4,148 +4,143 @@ import Restaurants from './components/restaurants'
 
 const App = () => {
 
-	// const apiUrl = "https://still-coast-01389.herokuapp.com/restaurants"
-	const apiUrl = "http://localhost:3000/restaurants"
+		// const apiUrl = "https://still-coast-01389.herokuapp.com/restaurants"
+		const apiUrl = "http://localhost:3000/restaurants"
 
-	// -------------------
-	// Hooks
-	// -------------------
-	const [newName, setNewName] = useState()
-	const [newAddress, setNewAddress] = useState()
-	const [newType, setNewType] = useState()
-	const [newChef, setNewChef] = useState()
-	const [newImage, setNewImage] = useState()
-	const [newRank, setNewRank] = useState()
-	const [restaurant, setRestaurant] = useState([])
-	const [newShow, setNewShow] = useState(false)
-	const [newShowRestaurant, setNewShowRestaurant] = useState(false)
-	const [newShowSearch, setNewShowSearch] = useState(false)
-	const [query, setQuery] = useState("")
+		// -------------------
+		// Hooks
+		// -------------------
+		const [newName, setNewName] = useState()
+		const [newAddress, setNewAddress] = useState()
+		const [newType, setNewType] = useState()
+		const [newChef, setNewChef] = useState()
+		const [newImage, setNewImage] = useState()
+		const [newRank, setNewRank] = useState()
+		const [restaurant, setRestaurant] = useState([])
+		const [newShow, setNewShow] = useState(false)
+		const [newShowRestaurant, setNewShowRestaurant] = useState(false)
+		const [newShowSearch, setNewShowSearch] = useState(false)
+		const [query, setQuery] = useState("")
 
-	// -------------------
-	// Handlers
-	// -------------------
+		// -------------------
+		// Handlers
+		// -------------------
 		const handleNewName = (event) => {
-        setNewName(event.target.value)
-    }
-    const handleNewAddress = (event) => {
-        setNewAddress(event.target.value)
-    }
-    const handleNewType = (event) => {
-        setNewType(event.target.value)
-    }
-    const handleNewChef = (event) => {
-        setNewChef(event.target.value)
-    }
-    const handleNewRank = (event) => {
-        setNewRank(event.target.value)
-    }
-    const handleNewImage = (event) => {
-        setNewImage(event.target.value)
-    }
-
-	const handleNewRestaurant = (event) => {
-    	event.preventDefault()
-			event.target.reset()
-    	axios.post(
-        `${apiUrl}`,
-			{
-				name: newName,
-				address: newAddress,
-				type: newType,
-				chef: newChef,
-				image: newImage,
-				rank: newRank,
-				show: newShow,
-			}
-    	).then(() => {
-		axios.get(`${apiUrl}`).then((response) => {
-			setRestaurant(response.data)
-			setNewShowRestaurant(false)
-		})
-    	})
-  	}
-
-	const handleDelete = (restaurantData) => {
-        	axios
-        	.delete(`${apiUrl}/${restaurantData._id}`)
-        	.then(() => {
-            axios
-            	.get(`${apiUrl}`)
-            	.then((response) => {
-                  setRestaurant(response.data)
-            })
-          })
-        }
-
-	const handleRestaurantUpdate = (event, restaurantData) => {
-	event.preventDefault()
-	axios
-		.put(
-		`${apiUrl}/${restaurantData._id}`,
-		{
-			name: newName,
-			address: newAddress,
-			type: newType,
-			chef: newChef,
-			image: newImage,
-			rank: newRank,
-			show: newShow,
+			setNewName(event.target.value)
 		}
-	)
-	.then(() => {
-		axios
-		.get(`${apiUrl}`)
-		.then((response) => {
-			setRestaurant(response.data)
-			setNewName()
-			setNewAddress()
-			setNewType()
-			setNewImage()
-			setNewChef()
-			setNewRank()
-			setNewShow(false)
-		})
-	})
-	}
+		const handleNewAddress = (event) => {
+			setNewAddress(event.target.value)
+		}
+		const handleNewType = (event) => {
+			setNewType(event.target.value)
+		}
+		const handleNewChef = (event) => {
+			setNewChef(event.target.value)
+		}
+		const handleNewRank = (event) => {
+			setNewRank(event.target.value)
+		}
+		const handleNewImage = (event) => {
+			setNewImage(event.target.value)
+		}
 
-	const show = (event, restaurantData) => {
-	event.preventDefault()
-		axios
-		.put(
-			`${apiUrl}/${restaurantData._id}`,
-			{
-				name: restaurantData.name,
-				address: restaurantData.address,
-				type: restaurantData.type,
-				chef: restaurantData.price,
-				image: restaurantData.image,
-				rank: restaurantData.rank,
-				show: !restaurantData.show,
-			}
-		)
-		.then(() => {
-		axios
-			.get(`${apiUrl}`)
-			.then((response) => {
-				setRestaurant(response.data)
+		const handleNewRestaurant = (event) => {
+			event.preventDefault()
+			event.target.reset()
+			axios.post(
+				`${apiUrl}`, {
+					name: newName,
+					address: newAddress,
+					type: newType,
+					chef: newChef,
+					image: newImage,
+					rank: newRank,
+					show: newShow,
+				}
+			).then(() => {
+				axios.get(`${apiUrl}`).then((response) => {
+					setRestaurant(response.data)
+					setNewShowRestaurant(false)
+				})
 			})
-		})
-	}
+		}
 
-	const showNewRestaurant = () => {
+		const handleDelete = (restaurantData) => {
+			axios
+				.delete(`${apiUrl}/${restaurantData._id}`)
+				.then(() => {
+					axios
+						.get(`${apiUrl}`)
+						.then((response) => {
+							setRestaurant(response.data)
+						})
+				})
+		}
+
+		const handleRestaurantUpdate = (event, restaurantData) => {
+			event.preventDefault()
+			axios
+				.put(
+					`${apiUrl}/${restaurantData._id}`, {
+						name: newName,
+						address: newAddress,
+						type: newType,
+						chef: newChef,
+						image: newImage,
+						rank: newRank,
+						show: newShow,
+					}
+				)
+				.then(() => {
+					axios
+						.get(`${apiUrl}`)
+						.then((response) => {
+							setRestaurant(response.data)
+							setNewName()
+							setNewAddress()
+							setNewType()
+							setNewImage()
+							setNewChef()
+							setNewRank()
+							setNewShow(false)
+						})
+				})
+		}
+
+		const show = (event, restaurantData) => {
+			event.preventDefault()
+			axios
+				.put(
+					`${apiUrl}/${restaurantData._id}`, {
+						name: restaurantData.name,
+						address: restaurantData.address,
+						type: restaurantData.type,
+						chef: restaurantData.price,
+						image: restaurantData.image,
+						rank: restaurantData.rank,
+						show: !restaurantData.show,
+					}
+				)
+				.then(() => {
+					axios
+						.get(`${apiUrl}`)
+						.then((response) => {
+							setRestaurant(response.data)
+						})
+				})
+		}
+
+		const showNewRestaurant = () => {
 			setNewShowRestaurant(!newShowRestaurant)
-
-	}
-	const showNewSearch = () => {
+		}
+		const showNewSearch = () => {
 			setNewShowSearch(!newShowSearch)
+		}
 
-	}
-
-	// -------------------
-	// Use Effect
-	// -------------------
-	useEffect(() => {
+		// -------------------
+		// Use Effect
+		// -------------------
+		useEffect(() => {
 			axios.get(`${apiUrl}`).then((response) => {
 				setRestaurant(response.data)
 			})
--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,46 +1,37 @@
 const Restaurants = (props) => {
 			return  (
 				<main>
-				<div>
 					<div>
-						<h1>{props.restaurants.name}</h1>
-						<h2># {props.restaurants.rank} In The World</h2>
-						<h2>{props.restaurants.address}</h2>
-						<h3>{props.restaurants.type}</h3>
-						<h4>{props.restaurants.chef}</h4>
-						<img src={props.restaurants.image}/>
+						<div>
+							<h1>{props.restaurants.name}</h1>
+							<h2># {props.restaurants.rank} In The World</h2>
+							<h2>{props.restaurants.address}</h2>
+							<h3>{props.restaurants.type}</h3>
+							<h4>{props.restaurants.chef}</h4>
+							<img src={props.restaurants.image}/>
+						</div>
+						<button onClick={(event) => {
+							props.handleDelete(props.restaurants)
+						}}>Delete Restaurant</button>
+						<button onClick={(event) => {
+							props.show(event, props.restaurants)
+						}}>Edit Info</button>
 					</div>
-					<button onClick={(event) => {
-						props.handleDelete(props.restaurants)
-					}}>Delete Restaurant</button>
-					<button onClick={(event) => {
-						props.show(event, props.restaurants)
-					}}>Edit Info</button>
-				</div>
-				<div>
-				{props.restaurants.show ?
-				<form onSubmit={(event) => {
-						props.handleRestaurantUpdate(event, props.restaurants)
-				}}>
-					<input onChange={props.handleNewName} defaultValue={props.restaurants.name}></input>
-
-					<input defaultValue={props.restaurants.address} onChange={props.handleNewAddress}></input>
-
-					<input defaultValue={props.restaurants.type} onChange={props.handleNewType}></input>
-
-					<input defaultValue={props.restaurants.image} onChange={props.handleNewImage}></input>
-
-					<input defaultValue={props.restaurants.rank} onChange={props.handleNewRank}></input>
-
-					<input defaultValue={props.restaurants.chef} onChange={props.handleNewChef}></input>
-					
-					<input type='submit' value='Submit'></input>
-					</form> : null }
-
-				</div>
+					<div>
+					{props.restaurants.show ?
+					<form onSubmit={(event) => {
+							props.handleRestaurantUpdate(event, props.restaurants)}}>
+						<input onChange={props.handleNewName} defaultValue={props.restaurants.name}></input>
+						<input defaultValue={props.restaurants.address} onChange={props.handleNewAddress}></input>
+						<input defaultValue={props.restaurants.type} onChange={props.handleNewType}></input>
+						<input defaultValue={props.restaurants.image} onChange={props.handleNewImage}></input>
+						<input defaultValue={props.restaurants.rank} onChange={props.handleNewRank}></input>
+						<input defaultValue={props.restaurants.chef} onChange={props.handleNewChef}></input>
+						<input type='submit' value='Submit'></input>
+						</form> : null }
+					</div>
 				</main>
 		)
 }
-
 
 export default Restaurants
